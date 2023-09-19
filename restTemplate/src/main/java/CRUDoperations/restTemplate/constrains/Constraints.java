016908096ca2ca30ec511a9f37cde6f05de686e2
Eshwar-24 <saieshwarmanchala42@gmail.com>
2023-09-20 00:23:52 +0530

    commit on rest

--- restTemplate/src/main/java/CRUDoperations/restTemplate/constrains/Constraints.java
+++ restTemplate/src/main/java/CRUDoperations/restTemplate/constrains/Constraints.java
@@ -4,5 +4,6 @@ import CRUDoperations.restTemplate.model.Product;
 
 public class Constraints {
     public static final String PRODUCT_URL="/products";
+    public static final String PRODUCT_URLs="/products";
     public static final String REST_TEMPLATE="/rest-template";
 }
